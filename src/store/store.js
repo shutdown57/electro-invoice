3dcd08a9fba98df3208d3bce771b907c051a8c6d
Sam Muse <m.shutdown57@gmail.com>
2019-09-20 08:19:32 +0430

    Added store file

---
+++ src/store/store.js
@@ -0,0 +1,35 @@
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+
+// import { createPersistedState, createSharedMutations } from 'vuex-electron'
+
+// import modules from './modules'
+
+// Vue.use(Vuex)
+
+// export default new Vuex.Store({
+//   modules,
+//   plugins: [
+//     createPersistedState(),
+//     createSharedMutations()
+//   ],
+//   strict: process.env.NODE_ENV !== 'production'
+// })
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import state from './state'
+import { mutations } from './mutations'
+import { actions } from './actions'
+import { getters } from './getters'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters,
+  actions
+})
+
