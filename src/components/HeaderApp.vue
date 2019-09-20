e1cad3626b43282c502af6b7e1da3ab6bf63ab4f
Sam Muse <m.shutdown57@gmail.com>
2019-09-20 12:26:22 +0430

    Added invoice and products to menu

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,15 +1,20 @@
 <template>
   <b-navbar>
     <template slot="end">
-      <b-navbar-dropdown label="Info">
-        <b-navbar-item tag="router-link" :to="{ path: '/about' }">About</b-navbar-item>
-        <b-navbar-item href="#">Contact</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/about' }">درباره</b-navbar-item>
+      <b-navbar-dropdown label="محصولات">
+        <b-navbar-item tag="router-link" :to="{ path: '/client/create' }">ایجاد مشتری</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/client/update' }">ویرایش مشتری</b-navbar-item>
+      </b-navbar-dropdown>
+      <b-navbar-dropdown label="فاکتورها">
+        <b-navbar-item tag="router-link" :to="{ path: '/client/create' }">ایجاد مشتری</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/client/update' }">ویرایش مشتری</b-navbar-item>
       </b-navbar-dropdown>
       <b-navbar-dropdown label="مشتریان">
-        <b-navbar-item tag="router-link" :to="{ path: '/client/create' }">ایجاد مشتری</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/clients/create' }">ایجاد مشتری</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/clients/' }">ویرایش مشتری</b-navbar-item>
       </b-navbar-dropdown>
-      <b-navbar-item href="#">Documentation</b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">Home</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">صفحه اصلی</b-navbar-item>
     </template>
   </b-navbar>
 </template>
