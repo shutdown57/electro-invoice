127469e1975c9b09bb0a70581fd89116597535e7
Sam Muse <m.shutdown57@gmail.com>
2019-09-20 12:29:54 +0430

    Added all clients page to route
    fixed prefix route for client

--- src/router.js
+++ src/router.js
@@ -1,38 +1,45 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-import Home from './views/Home.vue'
-import AddClient from './views/client/AddClient.vue'
-import UpdateClient from './views/client/UpdateClient.vue'
+import Home from "./views/Home.vue";
+import AddClient from "./views/client/AddClient.vue";
+import UpdateClient from "./views/client/UpdateClient.vue";
+import AllClients from "./views/client/AllClients.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/client/create',
-      name: 'AddClient',
-      component: AddClient
+      path: "/clients",
+      name: "AllClients",
+      component: AllClients
     },
     {
-      path: '/client/update',
-      name: 'UpdateClient',
+      path: "/clients/update",
+      name: "UpdateClient",
       component: UpdateClient
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/clients/create",
+      name: "AddClient",
+      component: AddClient
+    },
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
