d3ea34179b04153d79bb3690b99616b4ae152ce1
Sam Muse <m.shutdown57@gmail.com>
2019-09-20 12:34:49 +0430

    Added all client page

---
+++ src/views/client/AllClients.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="container">
+        <TableClients></TableClients>
+    </section>
+</template>
+
+<script>
+import TableClients from '@/components/client/TableClients.vue'
+
+export default{
+    name: 'AllClients',
+    components: {
+        TableClients
+    }
+}
+</script>
