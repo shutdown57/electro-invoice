710f47ad7334e44d591823a78ffa0b69ddd36eed
Sam Muse <m.shutdown57@gmail.com>
2019-09-20 08:17:52 +0430

    Added header component

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-navbar>
+    <template slot="end">
+      <b-navbar-dropdown label="Info">
+        <b-navbar-item tag="router-link" :to="{ path: '/about' }">About</b-navbar-item>
+        <b-navbar-item href="#">Contact</b-navbar-item>
+      </b-navbar-dropdown>
+      <b-navbar-dropdown label="مشتریان">
+        <b-navbar-item tag="router-link" :to="{ path: '/client/create' }">ایجاد مشتری</b-navbar-item>
+      </b-navbar-dropdown>
+      <b-navbar-item href="#">Documentation</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">Home</b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "HeaderApp"
+};
+</script>
+
+<style scoped>
+.rtld {
+  text-align: right;
+  direction: rtl;
+}
+</style>
