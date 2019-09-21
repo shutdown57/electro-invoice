cdcac23e51167d61acd7e50545a8102bc6a62da4
Sam Muse <m.shutdown57@gmail.com>
2019-09-21 13:19:02 +0430

    Added getProducts and getProduct
    cleanup

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,17 @@
 export const getters = {
+  // Clients
   getClient(state) {
     return state.oneClient;
   },
   getClients(state) {
     return state.allClients;
+  },
+
+  // Products
+  getProducts(state) {
+    return state.allProducts;
+  },
+  getProduct(state) {
+    return state.oneProduct;
   }
 };
