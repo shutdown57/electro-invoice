70c4db0d11f3734a39fce67683e55765bbd73c30
Sam Muse <m.shutdown57@gmail.com>
2019-09-21 13:17:45 +0430

    Cleanup
    added insertProduct, getProducts, addCurrentProduct and updateProduct

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,6 +2,8 @@ export const actions = {
   initializeDb({ commit }, client) {
     commit("INSERT_CLIENT", client);
   },
+
+  // Clients
   addCurrentClient({ commit }, client) {
     commit("ADD_CURRENT_CLIENT", client);
   },
@@ -12,6 +14,20 @@ export const actions = {
     commit("INSERT_CLIENT", data);
   },
   updateClient({ commit }, data) {
-    commit('UPDATE_CLIENT', data)
+    commit("UPDATE_CLIENT", data);
+  },
+
+  // Products
+  insertProduct({ commit }, product) {
+    commit("INSERT_PRODUCT", product);
+  },
+  getProducts({ commit }) {
+    commit("GET_PRODUCTS");
+  },
+  addCurrentProduct({ commit }, product) {
+    commit("ADD_CURRENT_PRODUCT", product);
+  },
+  updateProduct({ commit }, product) {
+    commit("UPDATE_PRODUCT", product);
   }
 };
