e5878de2e9dfedcb3783048f8992ee503186b4cd
Sam Muse <m.shutdown57@gmail.com>
2019-09-21 13:19:40 +0430

    Cleanup and Refactored updateUser to updateClient
    added insert product
    added get product
    added add current product
    added update product

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,7 @@
 import Database from "../mixin/Database";
 
 export const mutations = {
+  // Clients
   async INSERT_CLIENT(state, client) {
     await Database.insertClient(client);
   },
@@ -8,7 +9,7 @@ export const mutations = {
     state.allClients = await Database.getClients();
   },
   async UPDATE_CLIENT(state, client) {
-    await Database.updateUser(client)
+    await Database.updateClient(client);
   },
   ADD_CURRENT_CLIENT(state, client) {
     state.oneClient = {
@@ -20,5 +21,25 @@ export const mutations = {
       phone: client.phone,
       mobile: client.mobile
     };
+  },
+
+  // Products
+  async INSERT_PRODUCT(state, product) {
+    await Database.insertProduct(product);
+  },
+  async GET_PRODUCTS(state) {
+    state.allProducts = await Database.getProducts();
+  },
+  ADD_CURRENT_PRODUCT(state, product) {
+    state.oneProduct = {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      created: product.created,
+      updated: product.updated
+    };
+  },
+  async UPDATE_PRODUCT(state, product) {
+    await Database.updateProduct(product);
   }
 };
