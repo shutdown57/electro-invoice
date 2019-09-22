f6a6421964ef3a4650d554db1eca70d5f01a50c8
Sam Muse <m.shutdown57@gmail.com>
2019-09-22 09:50:17 +0330

    Added persian datetime picker package initialization

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
+import Buefy from 'buefy'
+import VuePersianDatetimePicker from 'vue-persian-datetime-picker'
+
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
-import Buefy from 'buefy'
 import './assets/scss/app.scss'
 
 Vue.use(Buefy)
+Vue.use(VuePersianDatetimePicker, {
+  name: 'VuePersianDatetimePicker',
+  props: {
+    inputFormat: 'YYYY-MM-DD',
+    format: 'jYYYY-jMM-jDD',
+    editable: false,
+    placeholder: 'انتخاب تاریخ',
+    autoSubmit: false
+  }
+})
 
 Vue.config.productionTip = false
 
