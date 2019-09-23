6061b3af28a2215e9bf30de12cc0170084c54e8e
Sam Muse <m.shutdown57@gmail.com>
2019-09-23 16:48:32 +0330

    Added calculate price for product in row component

---
+++ src/components/invoice/PriceCalculation.vue
@@ -0,0 +1,22 @@
+<template>
+      <b-input type="number" :value="priceCalculation()"></b-input>
+</template>
+
+<script>
+export default {
+  name: "PriceCalculation",
+  props: ["fee", "number"],
+  data() {
+    return {
+      price: 0
+    }
+  },
+  methods: {
+    priceCalculation: function () {
+      this.price = parseFloat(this.$props.fee) * parseFloat(this.$props.number)
+      this.$emit('rowPrice', this.price)
+      return this.price
+    }
+  }
+};
+</script>
