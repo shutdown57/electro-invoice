211895a1ea21237c4eff7b438336e0e21fbd5170
Sam Muse <m.shutdown57@gmail.com>
2019-09-23 16:30:43 +0330

    Added moment-jalaali and vue-persian-datetime-picker packages
    removed jalaali-js package

--- src/mixin/Database.js
+++ src/mixin/Database.js
@@ -1,7 +1,12 @@
 import * as sqlite from "sqlite3";
 import * as jalali from "jalaali-js";
-
+import * as mmj from 'moment-jalaali'
 // TODO: Implement error handling
+mmj.loadPersian({dialect: 'persian-modern'})
+let old = mmj(new Date()).format('jYYYY/jMM/jDD HH:mm')
+let new_ = mmj('1398/06/31 09:07')
+console.log(new_.diff(old, 'days'))
+console.log(mmj(new Date()).format('jYYYY/jMM/jDD HH:mm'))
 
 const init = () => {
   let db = new sqlite.Database("db.sqlite");
@@ -13,34 +18,40 @@ const init = () => {
             phone TEXT NOT NULL UNIQUE,
             mobile TEXT NOT NULL UNIQUE,
             created TEXT NOT NULL,
-            updated TEXT)`);
-
-    db.run(`CREATE TABLE IF NOT EXISTS products (
+            updated TEXT)`)
+      .run(`CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL UNIQUE,
             description TEXT,
             created TEXT NOT NULL,
-            updated TEXT,
-            invoice_id INTEGER)`);
-
-    db.run(`CREATE TABLE IF NOT EXISTS invoices (
+            updated TEXT)`)
+      .run(`CREATE TABLE IF NOT EXISTS invoices (
             id INTEGER PRIMARY KEY,
             description TEXT,
+            invoice_amount REAL NOT NULL,
+            damage_amount REAL NOT NULL,
+            transport_amount REAL NOT NULL,
+            total_amount REAL NOT NULL,
+            rent_period TEXT NOT NULL,
+            rent_start TEXT NOT NULL,
+            rent_end TEXT NOT NULL,
+            ceremony_address TEXT NOT NULL,
+            liquidation INTEGER DEFAULT 0,
             created TEXT NOT NULL,
             updated TEXT,
-            total REAL NOT NULL,
             user_id INTEGER,
-            FOREIGN KEY (user_id) REFERENCES users (id))`);
-
-    db.run(`CREATE TABLE IF NOT EXISTS invoice_product (
+            FOREIGN KEY (user_id) REFERENCES users (id))`)
+      .run(`CREATE TABLE IF NOT EXISTS invoice_product (
             id INTEGER,
-            user_id INTEGER,
+            fee REAL NOT NULL,
+            price REAL NOT NULL,
+            number INTEGER DEFAULT 0,
             invoice_id INTEGER,
             product_id INTEGER,
             PRIMARY KEY (invoice_id, product_id),
             FOREIGN KEY (invoice_id) REFERENCES invoices (id),
-            FOREIGN KEY (product_id) REFERENCES product (id))`);
-    db.run(`PRAGMA foreign_keys = ON`);
+            FOREIGN KEY (product_id) REFERENCES product (id))`)
+      .run(`PRAGMA foreign_keys = ON`);
   });
   db.close();
 };
@@ -59,7 +70,6 @@ const insertClient = ({ name, address, phone, mobile }) => {
       if (err) {
         return console.log(err.message);
       }
-      console.log(`A row has been inserted with rowid ${this.lastID}`);
     }
   );
   db.close();
@@ -114,7 +124,6 @@ const updateClient = ({ id, name, address, phone, mobile, created }) => {
     [name, address, phone, mobile, created, updated],
     function(err) {
       if (err) console.log(err.message);
-      console.log(`A row has been inserted with rowid ${this.lastID}`);
     }
   );
   db.close();
