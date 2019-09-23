3f22cb49b16774ec8e4bd2ce7f189fde6b8369ae
Sam Muse <m.shutdown57@gmail.com>
2019-09-23 16:33:29 +0330

    Changed VuePersianDatetimePicker component to commentout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
-import VuePersianDatetimePicker from 'vue-persian-datetime-picker'
+// import VuePersianDatetimePicker from 'vue-persian-datetime-picker'
 
 import App from './App.vue'
 import router from './router'
@@ -8,16 +8,16 @@ import store from './store/store'
 import './assets/scss/app.scss'
 
 Vue.use(Buefy)
-Vue.use(VuePersianDatetimePicker, {
-  name: 'VuePersianDatetimePicker',
-  props: {
-    inputFormat: 'YYYY-MM-DD',
-    format: 'jYYYY-jMM-jDD',
-    editable: false,
-    placeholder: 'انتخاب تاریخ',
-    autoSubmit: false
-  }
-})
+// Vue.use(VuePersianDatetimePicker, {
+//   name: 'VuePersianDatetimePicker',
+//   props: {
+//     inputFormat: 'YYYY-MM-DD',
+//     format: 'jYYYY-jMM-jDD',
+//     editable: false,
+//     placeholder: 'انتخاب تاریخ',
+//     autoSubmit: false
+//   }
+// })
 
 Vue.config.productionTip = false
 
