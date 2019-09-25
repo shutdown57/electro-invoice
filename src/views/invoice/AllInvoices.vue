e946d148f22d053c203448478e06229f0b592f18
Sam Muse <m.shutdown57@gmail.com>
2019-09-25 23:39:27 +0330

    Added show all invoices table page

---
+++ src/views/invoice/AllInvoices.vue
@@ -0,0 +1,114 @@
+<template>
+  <section>
+    <h1 class="has-text-centered title is-3">صفحه محصولات</h1>
+    <b-table
+      id="table-client"
+      :data="isEmpty(invoices) ? [] : invoices"
+      :paginated="isPaginated"
+      :per-page="perPage"
+      :current-page.sync="currentPage"
+      :pagination-simple="isPaginationSimple"
+      :pagination-position="paginationPosition"
+      :default-sort-direction="defaultSortDirection"
+      :sort-icon="sortIcon"
+      :bordered="isBordered"
+      :striped="isStriped"
+      :narrowed="isNarrowed"
+      :hoverable="isHoverable"
+      :sort-icon-size="sortIconSize"
+      :selected.sync="selected"
+      @click="addClient"
+      default-sort="user.first_name"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+      focusable
+    >
+      <template slot-scope="props">
+        <b-table-column field="id" label="شماره" width="40" sortable numeric>{{ props.row.id }}</b-table-column>
+
+        <b-table-column field="name" label="هزینه اجناس" sortable centered>{{ props.row.invoice_amount }}</b-table-column>
+        <b-table-column field="name" label="هزینه خسارت" sortable centered>{{ props.row.damage_amount }}</b-table-column>
+        <b-table-column field="name" label="هزینه حمل و نقل" sortable centered>{{ props.row.transport_amount }}</b-table-column>
+        <b-table-column field="name" label="هزینه کل" sortable centered>{{ props.row.total_amount }}</b-table-column>
+        <b-table-column field="name" label="مدت اجاره" sortable centered>{{ props.row.rent_period }}</b-table-column>
+        <b-table-column field="name" label="آدرس مراسم" sortable centered>{{ props.row.ceremony_address }}</b-table-column>
+        <b-table-column field="name" label="تسویه" sortable centered>{{ props.row.liquidation }}</b-table-column>
+        <b-table-column field="name" label="نام مشتری" sortable centered>{{ props.row.user_id }}</b-table-column>
+        
+        <b-table-column field="date" label="تاریخ شروع اجاره" sortable centered>
+          <span
+            class="tag is-info rtld"
+          >{{ props.row.rent_start ? new Date(props.row.rent_start).toLocaleDateString() : '----------' }}</span>
+        </b-table-column>
+        <b-table-column field="date" label="تاریخ پایان اجاره" sortable centered>
+          <span
+            class="tag is-info rtld"
+          >{{ props.row.rent_end ? new Date(props.row.rent_end).toLocaleDateString() : '----------' }}</span>
+        </b-table-column>
+
+        <b-table-column field="date" label="تاریخ بروز رسانی" sortable centered>
+          <span
+            class="tag is-info rtld"
+          >{{ props.row.updated ? new Date(props.row.updated).toLocaleDateString() : '----------' }}</span>
+        </b-table-column>
+
+        <b-table-column field="date" label="تاریخ ایجاد" sortable centered>
+          <span class="tag is-success rtld">{{ new Date(props.row.created).toLocaleDateString() }}</span>
+        </b-table-column>
+
+        <b-table-column
+          field="description"
+          label="توضیحات"
+          sortable
+          centered
+        >{{ props.row.description }}</b-table-column>
+      </template>
+    </b-table>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "AllInvoices",
+  data() {
+    return {
+      selected: {},
+      isPaginated: true,
+      isPaginationSimple: false,
+      paginationPosition: "bottom",
+      defaultSortDirection: "asc",
+      sortIcon: "arrow-up",
+      isBordered: true,
+      isStriped: true,
+      isNarrowed: false,
+      isHoverable: true,
+      sortIconSize: "is-small",
+      currentPage: 1,
+      perPage: 60
+    };
+  },
+  async mounted() {
+    await this.$store.dispatch("getInvoices");
+  },
+  computed: {
+    ...mapGetters({
+      invoices: "getInvoices"
+    })
+  },
+  methods: {
+    addClient(select) {
+    //   this.$store.dispatch("addCurrentProduct", select);
+    //   this.$router.push("/products/update");
+    },
+    isEmpty(arr) {
+      if (arr.length > 0) {
+        return false;
+      }
+      return true;
+    }
+  }
+};
+</script>
