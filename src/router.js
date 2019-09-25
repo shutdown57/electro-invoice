29c01b459d117b873465ba0b81ce133e94ebb90b
Sam Muse <m.shutdown57@gmail.com>
2019-09-25 23:26:55 +0330

    Added show all invoices table page route

--- src/router.js
+++ src/router.js
@@ -50,6 +50,11 @@ export default new Router({
       component: () => import("./views/invoice/AddInvoice.vue")
     },
     {
+      path: "/invoices",
+      name: "AllInvoices",
+      component: () => import("./views/invoice/AllInvoices.vue")
+    },
+    {
       path: "/about",
       name: "about",
       component: () => import("./views/About.vue")
