7a68962929e80211cd334c91388d19b43cef7b66
Sam Muse <m.shutdown57@gmail.com>
2019-09-28 16:06:35 +0330

    Fixed validation
    cleanup

--- src/views/product/AddProduct.vue
+++ src/views/product/AddProduct.vue
@@ -4,21 +4,13 @@
       <b-input
         dir="rtl"
         type="text"
-        pattern="(.*){3,}"
-        validation-message="نام و نام خانوادگی مشتری به زبان فارسی"
+        pattern="(.*){2,}"
+        validation-message="نام مصحول باید بیش از ۲ حرف باشد"
         v-model="newProduct.name"
         rounded
         required
       ></b-input>
     </b-field>
-
-    <!-- <b-field class="has-text-right" label="نام مشتری">
-      <b-select placeholder="انتخاب مشتری" icon="account" rounded required>
-        <option dir="rtl" v-for="client in clients" :value="client.id" :key="client.id">
-            {{ client.name }}
-        </option>
-      </b-select>
-    </b-field>-->
 
     <b-field class="has-text-right" label="توضیحات">
       <b-input dir="rtl" type="textarea" v-model="newProduct.description"></b-input>
@@ -30,7 +22,6 @@
 </template>
 
 <script>
-// FIXME: validation message
 import { mapGetters } from "vuex";
 export default {
   name: "AddProduct",
