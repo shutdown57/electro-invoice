7b8a74b31437b1563e87342bac5383b5dae82491
Sam Muse <m.shutdown57@gmail.com>
2019-10-03 09:51:42 +0330

    Added single client show page

---
+++ src/views/client/SingleClient.vue
@@ -0,0 +1,54 @@
+<template>
+  <section dir="rtl" class="container">
+    <form id="single-client">
+      <b-field horizontal label="نام و نام خانوادگی">
+        <b-input name="subject" v-model="client.name" expanded disabled></b-input>
+      </b-field>
+
+      <b-field horizontal label="تلفن ثابت">
+        <b-input name="subject" v-model="client.phone" expanded disabled></b-input>
+      </b-field>
+
+      <b-field horizontal label="تلفن همراه">
+        <b-input name="subject" v-model="client.mobile" expanded disabled></b-input>
+      </b-field>
+
+      <b-field horizontal label="آدرس">
+        <b-input type="textarea" v-model="client.address" disabled></b-input>
+      </b-field>
+    </form>
+    <br />
+    <b-field>
+      <p class="control has-text-centered">
+        <button class="button is-medium is-success" @click="print()">چاپ</button>
+      </p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <p class="control has-text-centered">
+        <button class="button is-medium is-warning" @click="updateClient()">ویرایش</button>
+      </p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <p class="control has-text-centered">
+        <button class="button is-medium is-danger">حذف</button>
+      </p>
+    </b-field>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "SingleClient",
+  computed: {
+    ...mapGetters({
+      client: "getClient"
+    })
+  },
+  methods: {
+    updateClient() {
+      this.$router.push("/clients/update");
+    },
+    print() {
+      this.$store.dispatch("printOneCustomer", this.client);
+    }
+  }
+};
+</script>
