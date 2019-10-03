9ee4b4943749185acfe4f4b8bbf22ba0ec13c9d7
Sam Muse <m.shutdown57@gmail.com>
2019-10-03 09:47:59 +0330

    Added print one customer method
    fixed print all customers method name

--- src/mixin/Print.js
+++ src/mixin/Print.js
@@ -1,31 +1,52 @@
 // Documentation: https://printjs.crabbly.com/
-import printJS from "print-js"
+import printJS from "print-js";
 
-const printCustomers = (data) => {
-    let json = []
-    data.forEach(el => {
-      json.push({
-        'شماره': el.id,
-        'نام': el.name,
-        'تلفن ثابت': el.phone,
-        'تلفن همراه': el.mobile,
-        'تاریخ ایجاد': el.created,
-        'تاریخ بروزرسانی': el.updated,
-        'آدرس': el.address
-      })
+const oneCustomer = () => {
+  printJS({
+    printable: "single-client",
+    type: "html",
+    repeatTableHeader: false,
+    gridHeaderStyle: "color: black;  border: 1px solid #3971A5;",
+    gridStyle: "border: 1px solid #3971A5;  text-align: center;",
+    header: ``,
+    style: ``
+  });
+};
+
+const allCustomers = data => {
+  let json = [];
+  data.forEach(el => {
+    json.push({
+      شماره: el.id,
+      نام: el.name,
+      "تلفن ثابت": el.phone,
+      "تلفن همراه": el.mobile,
+      "تاریخ ایجاد": el.created,
+      "تاریخ بروزرسانی": el.updated,
+      آدرس: el.address
     });
-    printJS({
-      printable: json,
-      type: 'json',
-      properties: ['شماره', 'نام', 'تلفن ثابت', 'تلفن همراه', 'تاریخ ایجاد', 'تاریخ بروزرسانی', 'آدرس'],
-      repeatTableHeader: true,
-      gridHeaderStyle: 'color: black;  border: 1px solid #3971A5;',
-      gridStyle: 'border: 1px solid #3971A5;  text-align: center;',
-      header: '<h2 class="custom-h3">PDF data</h2>',
-      style: '.custom-h3 { color: red; }'
-    })
-}
+  });
+  printJS({
+    printable: json,
+    type: "json",
+    properties: [
+      "شماره",
+      "نام",
+      "تلفن ثابت",
+      "تلفن همراه",
+      "تاریخ ایجاد",
+      "تاریخ بروزرسانی",
+      "آدرس"
+    ],
+    repeatTableHeader: true,
+    gridHeaderStyle: "color: black;  border: 1px solid #3971A5;",
+    gridStyle: "border: 1px solid #3971A5;  text-align: center;",
+    header: '<h2 class="custom-h3">PDF data</h2>',
+    style: ".custom-h3 { color: red; }"
+  });
+};
 
 export default {
-    printCustomers
-}
+  allCustomers,
+  oneCustomer
+};
