a3816e98f1cd490a82dcc50b9686df0958a9e374
Sam Muse <m.shutdown57@gmail.com>
2019-10-03 10:40:19 +0330

    Added print one product and all products methods

--- src/mixin/Print.js
+++ src/mixin/Print.js
@@ -1,6 +1,8 @@
 // Documentation: https://printjs.crabbly.com/
 import printJS from "print-js";
 
+/* ################################################################################## */
+/* Client */
 const oneCustomer = () => {
   printJS({
     printable: "single-client",
@@ -46,7 +48,50 @@ const allCustomers = data => {
   });
 };
 
+/* ################################################################################## */
+/* Product */
+const oneProduct = () => {
+  printJS({
+    printable: "single-product",
+    type: "html",
+    repeatTableHeader: false,
+    gridHeaderStyle: "color: black;  border: 1px solid #3971A5;",
+    gridStyle: "border: 1px solid #3971A5;  text-align: center;",
+    header: ``,
+    style: ``
+  });
+}
+
+const allProducts = data => {
+  let json = [];
+  data.forEach(el => {
+    json.push({
+      "شماره": el.id,
+      "نام محصول": el.name,
+      "تاریخ ایجاد": el.created,
+      "تاریخ بروزرسانی": el.updated
+    });
+  });
+  printJS({
+    printable: json,
+    type: "json",
+    properties: [
+      "شماره",
+      "نام محصول",
+      "تاریخ ایجاد",
+      "تاریخ بروزرسانی"
+    ],
+    repeatTableHeader: true,
+    gridHeaderStyle: "color: black;  border: 1px solid #3971A5;",
+    gridStyle: "border: 1px solid #3971A5;  text-align: center;",
+    header: '<h2 class="custom-h3">PDF data</h2>',
+    style: ".custom-h3 { color: red; }"
+  });
+}
+
 export default {
   allCustomers,
-  oneCustomer
+  oneCustomer,
+  oneProduct,
+  allProducts
 };
