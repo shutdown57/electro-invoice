ec2486d74d838c157be3d9c9e150fb8dda4bd603
Sam Muse <m.shutdown57@gmail.com>
2019-10-03 09:50:59 +0330

    Added print button and method

--- src/views/client/AllClients.vue
+++ src/views/client/AllClients.vue
@@ -1,16 +1,32 @@
 <template>
-    <section class="container">
-        <TableClients></TableClients>
-    </section>
+  <section class="container">
+    <TableClients></TableClients>
+    <b-field>
+      <p class="control has-text-centered">
+        <button class="button is-medium is-success" @click="print()">چاپ</button>
+      </p>
+    </b-field>
+  </section>
 </template>
 
 <script>
-import TableClients from '@/components/client/TableClients.vue'
+import TableClients from "@/components/client/TableClients.vue";
+import { mapGetters } from 'vuex';
 
-export default{
-    name: 'AllClients',
-    components: {
-        TableClients
-    }
-}
+export default {
+  name: "AllClients",
+  components: {
+    TableClients
+  },
+  computed: {
+      ...mapGetters({
+          clients: 'getClients'
+      })
+  },
+  methods: {
+      print() {
+          this.$store.dispatch('printAllCustomers', this.clients)
+      }
+  }
+};
 </script>
