ca475dcd37f0bb9ed7a18b7d67ddd5893fbef76c
Sam Muse <m.shutdown57@gmail.com>
2019-10-04 14:17:26 +0330

    Fixed rtl show menu
    fixed menu names

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -2,17 +2,17 @@
   <b-navbar>
     <template slot="end">
       <b-navbar-item tag="router-link" :to="{ path: '/about' }">درباره</b-navbar-item>
-      <b-navbar-dropdown label="محصولات">
+      <b-navbar-dropdown class="rtld" label="محصولات">
         <b-navbar-item tag="router-link" :to="{ path: '/products/create' }">ایجاد محصول</b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/products' }">ویرایش محصول</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/products' }">نمایش لیست محصولات</b-navbar-item>
       </b-navbar-dropdown>
-      <b-navbar-dropdown label="فاکتورها">
+      <b-navbar-dropdown class="rtld" label="فاکتورها">
         <b-navbar-item tag="router-link" :to="{ path: '/invoices/create' }">ایجاد فاکتور</b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/invoices' }">ویرایش فاکتور</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/invoices' }">نمایش لیست فاکتورها</b-navbar-item>
       </b-navbar-dropdown>
-      <b-navbar-dropdown label="مشتریان">
+      <b-navbar-dropdown class="rtld" label="مشتریان">
         <b-navbar-item tag="router-link" :to="{ path: '/clients/create' }">ایجاد مشتری</b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/clients/' }">ویرایش مشتری</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/clients/' }">نمایش لیست مشتریان</b-navbar-item>
       </b-navbar-dropdown>
       <b-navbar-item tag="router-link" :to="{ path: '/' }">صفحه اصلی</b-navbar-item>
     </template>
