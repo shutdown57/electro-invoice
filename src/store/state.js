152e63ab294de392e85290f7c578803778f1a507
Sam Muse <m.shutdown57@gmail.com>
2019-10-07 11:19:21 +0330

    Added liquidationInvoices and clientInvoices arrays

--- src/store/state.js
+++ src/store/state.js
@@ -5,5 +5,7 @@ export default {
     allProducts: [],
     oneInvoice: {},
     allInvoices: [],
-    invoiceProducts: []
+    invoiceProducts: [],
+    liquidationInvoices: [],
+    clientInvoices: []
 }
