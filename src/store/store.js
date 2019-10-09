95885db601971897fbc1b67c47bac01507446eb5
Sam Muse <m.shutdown57@gmail.com>
2019-10-09 10:56:49 +0330

    Cleanup

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,18 @@
 // Guide: https://jasonwatmore.com/post/2018/07/14/vue-vuex-user-registration-and-login-tutorial-example
 // Documentation: https://vuex.vuejs.org/guide/modules.html
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import state from './state'
-import { mutations } from './mutations'
-import { actions } from './actions'
-import { getters } from './getters'
+import state from "./state";
+import { mutations } from "./mutations";
+import { actions } from "./actions";
+import { getters } from "./getters";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state,
   mutations,
   getters,
   actions
-})
-
+});
