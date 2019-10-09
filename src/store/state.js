742756aa7dcf740732fca3c08ebd958dcd5114a5
Sam Muse <m.shutdown57@gmail.com>
2019-10-09 10:56:24 +0330

    Cleanup

--- src/store/state.js
+++ src/store/state.js
@@ -1,11 +1,11 @@
 export default {
-    oneClient: {},
-    allClients: [],
-    oneProduct: {},
-    allProducts: [],
-    oneInvoice: {},
-    allInvoices: [],
-    invoiceProducts: [],
-    liquidationInvoices: [],
-    clientInvoices: []
-}
+  oneClient: {},
+  allClients: [],
+  oneProduct: {},
+  allProducts: [],
+  oneInvoice: {},
+  allInvoices: [],
+  invoiceProducts: [],
+  liquidationInvoices: [],
+  clientInvoices: []
+};
