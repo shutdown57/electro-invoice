630e20b45830110ff85a847f2a04f36cb9c78061
Sam Muse <m.shutdown57@gmail.com>
2019-10-13 10:44:06 +0330

    Added info content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="section rtld">
+    <h1 class="title is-1 has-text-centered">درباره</h1>
+    <div class="container">
+      <h2 class="title is-3">نرم افزار مدیریت فاکتور</h2>
+      <p>توسعه دهنده: <strong>مجید محمدی پرور</strong></p>
+      <p>ایمیل: <strong>m.shutdown57@gmail.com</strong></p>
+      <p>شماره تماس: <strong>09227056365</strong></p>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.rtld {
+  direction: rtl;
+  text-align: right;
+}
+</style>
 
