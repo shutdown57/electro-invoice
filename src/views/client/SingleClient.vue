8461dd5cf89767a7c40f744f0be0efa7877fb43e
Sam Muse <m.shutdown57@gmail.com>
2020-03-02 17:50:44 +0330

    Cleanup

--- src/views/client/SingleClient.vue
+++ src/views/client/SingleClient.vue
@@ -2,17 +2,32 @@
   <section dir="rtl" class="container">
     <form id="single-client">
       <b-field horizontal label="نام و نام خانوادگی">
-        <b-input name="subject" v-model="client.name" expanded disabled></b-input>
+        <b-input
+          name="subject"
+          v-model="client.name"
+          expanded
+          disabled
+        ></b-input>
       </b-field>
-      
+
       <b-field horizontal label="تلفن ثابت">
-        <b-input name="subject" v-model="client.phone" expanded disabled></b-input>
+        <b-input
+          name="subject"
+          v-model="client.phone"
+          expanded
+          disabled
+        ></b-input>
       </b-field>
-      
+
       <b-field horizontal label="تلفن همراه">
-        <b-input name="subject" v-model="client.mobile" expanded disabled></b-input>
+        <b-input
+          name="subject"
+          v-model="client.mobile"
+          expanded
+          disabled
+        ></b-input>
       </b-field>
-      
+
       <b-field horizontal label="آدرس">
         <b-input type="textarea" v-model="client.address" disabled></b-input>
       </b-field>
@@ -20,16 +35,24 @@
     <br />
     <b-field>
       <p class="control has-text-centered">
-        <button class="button is-medium is-success" @click="print()">چاپ</button>
-      </p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button class="button is-medium is-success" @click="print()">
+          چاپ
+        </button>
+      </p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <p class="control has-text-centered">
-        <button class="button is-medium is-warning" @click="updateClient()">ویرایش</button>
-      </p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button class="button is-medium is-warning" @click="updateClient()">
+          ویرایش
+        </button>
+      </p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <p class="control has-text-centered">
-        <button class="button is-medium is-danger" @click="deleteClient()">حذف</button>
+        <button class="button is-medium is-danger" @click="deleteClient()">
+          حذف
+        </button>
       </p>
     </b-field>
-    <hr>
+    <hr />
     <section dir="ltr">
       <h3 class="title is-4 has-text-centered">لیست فاکتورهای این مشتری</h3>
       <ClientInvoices></ClientInvoices>
@@ -39,7 +62,7 @@
 
 <script>
 import { mapGetters } from "vuex";
-import ClientInvoices from "@/components/invoice/ClientInvoices"
+import ClientInvoices from "@/components/invoice/ClientInvoices";
 
 export default {
   name: "SingleClient",
