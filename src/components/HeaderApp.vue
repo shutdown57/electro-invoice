41bc6c5e946e056f7b4a9cce3e8bcfd0c97b7b3d
Sam Muse <m.shutdown57@gmail.com>
2020-03-02 17:44:00 +0330

    Cleanup

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -4,26 +4,42 @@
       <SearchInvoiceId></SearchInvoiceId>
     </template>
     <template slot="end">
-      <b-navbar-item tag="router-link" :to="{ path: '/about' }">درباره</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/about' }">
+        درباره
+      </b-navbar-item>
       <b-navbar-dropdown class="rtld" label="محصولات">
-        <b-navbar-item tag="router-link" :to="{ path: '/products/create' }">ایجاد محصول</b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/products' }">نمایش لیست محصولات</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/products/create' }">
+          ایجاد محصول
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/products' }">
+          نمایش لیست محصولات
+        </b-navbar-item>
       </b-navbar-dropdown>
       <b-navbar-dropdown class="rtld" label="فاکتورها">
-        <b-navbar-item tag="router-link" :to="{ path: '/invoices/create' }">ایجاد فاکتور</b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/invoices' }">نمایش لیست فاکتورها</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/invoices/create' }">
+          ایجاد فاکتور
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/invoices' }">
+          نمایش لیست فاکتورها
+        </b-navbar-item>
       </b-navbar-dropdown>
       <b-navbar-dropdown class="rtld" label="مشتریان">
-        <b-navbar-item tag="router-link" :to="{ path: '/clients/create' }">ایجاد مشتری</b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/clients/' }">نمایش لیست مشتریان</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/clients/create' }">
+          ایجاد مشتری
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/clients/' }">
+          نمایش لیست مشتریان
+        </b-navbar-item>
       </b-navbar-dropdown>
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">صفحه اصلی</b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        صفحه اصلی
+      </b-navbar-item>
     </template>
   </b-navbar>
 </template>
 
 <script>
-import SearchInvoiceId from '@/components/search/SearchInvoiceId';
+import SearchInvoiceId from "@/components/search/SearchInvoiceId";
 export default {
   name: "HeaderApp",
   components: {
@@ -38,3 +54,4 @@ export default {
   direction: rtl;
 }
 </style>
+
