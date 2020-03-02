0dc942ff2cb318ade42e46bd837772127dafa3be
Sam Muse <m.shutdown57@gmail.com>
2020-03-02 17:54:50 +0330

    Cleanup

--- src/views/product/SingleProduct.vue
+++ src/views/product/SingleProduct.vue
@@ -2,35 +2,67 @@
   <section dir="rtl" class="container">
     <form id="single-product">
       <b-field horizontal label="نام محصول">
-        <b-input name="subject" v-model="product.name" expanded disabled></b-input>
+        <b-input
+          name="subject"
+          v-model="product.name"
+          expanded
+          disabled
+        ></b-input>
       </b-field>
 
       <b-field horizontal label="قیمت محصول">
-        <b-input name="subject" v-model="product.price" expanded disabled></b-input>
+        <b-input
+          name="subject"
+          v-model="product.price"
+          expanded
+          disabled
+        ></b-input>
       </b-field>
 
       <b-field horizontal label="تاریخ ایجاد">
-        <b-input name="subject" v-model="product.created" expanded disabled></b-input>
+        <b-input
+          name="subject"
+          v-model="product.created"
+          expanded
+          disabled
+        ></b-input>
       </b-field>
 
       <b-field horizontal label="تاریخ ویرایش">
-        <b-input name="subject" v-model="product.updated" expanded disabled></b-input>
+        <b-input
+          name="subject"
+          v-model="product.updated"
+          expanded
+          disabled
+        ></b-input>
       </b-field>
 
       <b-field horizontal label="توضیحات">
-        <b-input type="textarea" v-model="product.description" disabled></b-input>
+        <b-input
+          type="textarea"
+          v-model="product.description"
+          disabled
+        ></b-input>
       </b-field>
     </form>
     <br />
     <b-field>
       <p class="control has-text-centered">
-        <button class="button is-medium is-success" @click="print()">چاپ</button>
-      </p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button class="button is-medium is-success" @click="print()">
+          چاپ
+        </button>
+      </p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <p class="control has-text-centered">
-        <button class="button is-medium is-warning" @click="updateProduct()">ویرایش</button>
-      </p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button class="button is-medium is-warning" @click="updateProduct()">
+          ویرایش
+        </button>
+      </p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <p class="control has-text-centered">
-        <button class="button is-medium is-danger" @click="deleteProduct()">حذف</button>
+        <button class="button is-medium is-danger" @click="deleteProduct()">
+          حذف
+        </button>
       </p>
     </b-field>
   </section>
